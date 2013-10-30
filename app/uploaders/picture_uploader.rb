573d6e815dc30875533c21c65d466f0f04e463dc
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-10-30 00:20:44 -0700

    Change the Idea index page so that only images and their captions load.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,10 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  version :thumb do
-    process :resize_to_fill => [50, 50]
-  end
-
+  
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -35,15 +32,19 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
+  
+  version :medium do
+    process :resize_to_fit => [300, 200]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
